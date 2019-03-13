fce1e74d89fc27918f8b5bc098bf917be912340a
steve <steve@localhost.localdomain>
2019-03-13 00:33:33 -0400

    check from list of keywords case insensitively

--- src/Main.hs
+++ src/Main.hs
@@ -9,11 +9,15 @@ import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as TIO
 import qualified Discord                       as DSC
 import           Data.Either
+
 main :: IO ()
 main = do
   token   <- T.strip <$> TIO.readFile "./auth-token.secret"
   discord <- DSC.loginRestGateway (DSC.Auth token)
   finally (loopingMain discord) (DSC.stopDiscord discord)
+
+keywords :: [T.Text]
+keywords = ["johnny", "sins"]
 
 loopingMain :: (DSC.RestChan, DSC.Gateway, z) -> IO ()
 loopingMain dis = do
@@ -21,19 +25,26 @@ loopingMain dis = do
   case e of
     Left  er                    -> putStrLn ("Event error: " <> show er)
     Right (DSC.MessageCreate m) -> do
-      when (isPing (DSC.messageText m)) $ do
-        resp <- DSC.restCall
-          dis
-          (DSC.CreateMessage (DSC.messageChannel m) "Pong!")
-        print resp
-        putStrLn ""
+      when (isKeyword (DSC.messageText m) && not (fromBot m)) $ do
+        let message   = DSC.messageText m
+        let channelID = DSC.messageChannel m
+        let response  = generateResponse message
+        _ <- sendChannelMessage channelID $ generateResponse message
+        return ()
       loopingMain dis
     _ -> loopingMain dis
+ where
+  sendChannelMessage channelID message =
+    DSC.restCall dis (DSC.CreateMessage channelID message)
 
 fromBot :: DSC.Message -> Bool
 fromBot m = case DSC.messageAuthor m of
   Right u          -> DSC.userIsBot u
   Left  _webhookid -> True
 
-isPing :: T.Text -> Bool
-isPing = T.isPrefixOf "ping" . T.map toLower
+isKeyword :: T.Text -> Bool
+isKeyword message = any (`caseInsensitiveIsInfix` message) keywords
+  where caseInsensitiveIsInfix key str = T.isInfixOf key $ T.toLower str
+
+generateResponse :: T.Text -> T.Text
+generateResponse message = ":poop:"
