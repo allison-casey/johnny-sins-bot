0c2b1a95b466525e6211d218aba128bc5c202c94
steve <steve@localhost.localdomain>
2019-03-13 22:35:59 -0400

    added function documentation

--- src/Main.hs
+++ src/Main.hs
@@ -35,6 +35,8 @@ data BotConfig = BotConfig
   , probability :: Double
   , triggers :: [BotTrigger] } deriving (Show, Generic, ToJSON, FromJSON)
 
+-- | Kicks of the primary event loop for the bot, initializing all necessary
+-- connections and authentication tokens.
 main :: IO ()
 main = do
   rawJSON <- B.readFile "./config.json"
@@ -46,6 +48,8 @@ main = do
       discord <- DSC.loginRestGateway (DSC.Auth token)
       finally (loopingMain conf discord) (DSC.stopDiscord discord)
 
+-- | Primary event loop for bot actions. Will recieve all messages in a server
+-- and respond with a quote from your boi Johnny Sins
 loopingMain :: BotConfig -> (DSC.RestChan, DSC.Gateway, z) -> IO ()
 loopingMain conf dis = do
   e <- DSC.nextEvent dis
@@ -78,20 +82,25 @@ loopingMain conf dis = do
     _ <- DSC.restCall dis (DSC.CreateMessage channelID message)
     return ()
 
+-- | Checks if a message is from a bot
 fromBot :: DSC.Message -> Bool
 fromBot m = case DSC.messageAuthor m of
   Right u          -> DSC.userIsBot u
   Left  _webhookid -> True
 
+-- | Finds the trigger responsible for handling the given message
 getResponsibleTrigger :: [BotTrigger] -> T.Text -> Maybe BotTrigger
 getResponsibleTrigger triggers message = find checkTrigger triggers
  where
   checkTrigger trigger =
     any (`caseInsensitiveIsInfix` message) (keywords trigger)
 
+-- | A case insensitive version of isInfixOf
 caseInsensitiveIsInfix :: T.Text -> T.Text -> Bool
 caseInsensitiveIsInfix key str = T.isInfixOf key $ T.toLower str
 
+-- | Checks if the message contains any of the key words defined in the
+-- bot triggers list
 isKeyword :: [BotTrigger] -> T.Text -> Bool
 isKeyword triggers message = any checkTriggerKeywords triggers
  where
@@ -99,11 +108,14 @@ isKeyword triggers message = any checkTriggerKeywords triggers
   checkTriggerKeywords trigger =
     any (`caseInsensitiveIsInfix` message) (keywords trigger)
 
+-- | Pulls a random response from the trigger config
 generateResponse :: BotTrigger -> IO T.Text
 generateResponse trigger = (responsesList !!)
   <$> randomRIO (0, length responsesList - 1)
   where responsesList = responses trigger
 
+-- | Based on a given probability. randomly determine if the bot
+-- should respond
 shouldRespond :: Double -> IO Bool
 shouldRespond prob = do
   chance <- randomIO :: IO Double
